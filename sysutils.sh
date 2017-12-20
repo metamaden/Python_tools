d970cd68fcd20621814d46a892614e1177962b2b
smaden <metamaden@users.noreply.github.com>
2017-12-20 10:12:28 -0800

    Update sysutils.sh

--- sysutils.sh
+++ sysutils.sh
@@ -1,4 +1,4 @@
-''' These are utility functions to manipulate, install, and run python and python modules '''
+# These are utility functions to manipulate, install, and run python and python modules
 
-''' view the path to you python version and modules '''
+# view the path to you python version and modules
 python -c "import sys; print(sys.path)"
